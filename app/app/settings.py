7bdf3dc4abd1b4951c24b95faf7d0a46c12da9a4
ramosdeluis <alberto.rir@icloud.com>
2023-06-15 16:58:34 -0300

    Deply config.

--- app/app/settings.py
+++ app/app/settings.py
@@ -9,14 +9,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-q#$t5+d3)_d7qxdjkfat85wsmjyt7$pocwnq(50@g1jl8i(six"
-)
+SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
-ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get("ALLOWED_HOSTS", "").split(","),
+    )
+)
 
 
 # Application definition
